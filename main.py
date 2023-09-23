b1eadb0e4ae65bdfd7593710d2ad04390f6c3fd3
zjr <a3502900767@gmail.com>
2023-09-23 17:40:30 +0800

    fix:限定参数r为自然数

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ def main():
     start = time.time()
     # 输入文件绝对路径，arg1，arg2为要查重的文件，arg3为答案文件
     parser = argparse.ArgumentParser(description="四则运算生成器")
-    parser.add_argument('-n', default=100, type=str, help='生成题目的个数')
-    parser.add_argument('-r', default=10, type=str, help='生成题目中数值的范围')
+    parser.add_argument('-n', default=10, type=str, help='生成题目的个数')
+    parser.add_argument('-r', default='p', type=str, help='生成题目中数值的范围')
     parser.add_argument('-e', type=str, default=" ", help='题目文件')
     parser.add_argument('-a', type=str, default=" ", help='答案文件')
     args = parser.parse_args()
+    # 把r限制在自然数范围内
+    # 使用异常处理来确保r是一个数字
+    try:
+        try:
+            if args.r > 0:
+                pass
+            else:
+                print(f"{args.r} 不是自然数（正整数）")
+        except TypeError:
+            print("请输入一个数")
+
+    except ValueError:
+        print("输入不是有效的整数")
+
     n, r, e, a = int(args.n), int(args.r), args.e.strip(), args.a.strip()
     product = Generator(r, n)
     questions = product.problemArray
